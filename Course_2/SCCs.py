f1574f4041c95aedb8e5f873dcd5fabd0feea7ee
Yilei He <yileihe1989@hotmail.com>
2018-08-19 13:38:28 -0700

    modify SCCs

--- Course_2/SCCs.py
+++ Course_2/SCCs.py
@@ -26,28 +26,44 @@ def r_graph(graph):
             reversed_graph[neighbor].append(node)
     return reversed_graph
 
+
 def dfs_scc(graph, node, visited, scc):
     visited[node] = True
-    for neighbor in graph[node]:
-        if not visited[neighbor] and neighbor != node:
-            dfs_scc(graph, neighbor, visited, scc)
+    if node in graph:
+        for neighbor in graph[node]:
+            if not visited[neighbor] and neighbor != node:
+                dfs_scc(graph, neighbor, visited, scc)
     scc.append(node)
 
 
 if __name__ == "__main__":
     graph = {
-        "1": ["0"],
-        "2": ["3"],
-        "0": ["0", "2", "9"],
-        "3": ["1"],
-        "9": ["5"],
-        "5": ["7", "4", "10"],
-        "7": ["5", "9"],
+        "1": ["3"],
+        "3": ["4", "5", "6"],
+        "5": [],
         "4": [],
-        "10": ["5"]
+        "6": ["3", "7"],
+        "7": ["8"],
+        "8": ["6"]
     }
 
-    visited = {i: False for i in graph.keys()}
+    # graph = {
+    #     "1": ["0"],
+    #     "2": ["3"],
+    #     "0": ["0", "2", "9"],
+    #     "3": ["1"],
+    #     "9": ["5"],
+    #     "5": ["7", "4", "10"],
+    #     "7": ["5", "9"],
+    #     "4": [],
+    #     "10": ["5"]
+    # }
+
+    node_set = set(graph.keys())
+    for i, j in graph.items():
+        node_set |= set(j)
+
+    visited = {i: False for i in node_set}
     nodes_stack = []
     for node in graph.keys():
         if not visited[node]:
@@ -55,9 +71,10 @@ if __name__ == "__main__":
 
     reversed_graph = r_graph(graph)
 
-    visited = {i: False for i in reversed_graph.keys()}
     scc_list = []
+    visited = {i: False for i in node_set}
     for node in nodes_stack:
+        print(node)
         if not visited[node]:
             scc = []
             dfs_scc(reversed_graph, node, visited, scc)
